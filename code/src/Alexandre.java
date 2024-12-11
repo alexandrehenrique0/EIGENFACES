130be241e52ac3bd54df259ca063be2ad5b08a09
Alexandre Henrique <1240720@isep.ipp.pt>
2024-12-11 10:03:54 +0000

    Classe Alexandre
    Coluna Media Variancias
    VerificaçãoEAM

--- code/src/Alexandre.java
+++ code/src/Alexandre.java
@@ -1,5 +1,8 @@
 public class Alexandre {
     // Calcula o Erro Absoluto Médio (EAM)
+    public static void main(String[] args) {
+
+    }
     public static double calculateEAM(double[][] A, double[][] Ak) {
         int M = A.length;
         int N = A[0].length;
@@ -14,5 +17,33 @@ public class Alexandre {
         // Calcula o erro médio
         return erroAbsMed / (M * N);
     }
-}
 
+    public static boolean testCalculateEAM(double[][] A, double[][] Ak, double expectedEAM) {
+        // Chama a função que calcula o EAM
+        double calculatedEAM = calculateEAM(A, Ak);
+
+        // Compara o valor calculado com o esperado
+        return calculatedEAM == expectedEAM;
+    }
+
+    public static void covariancias() {
+
+    }
+
+    public static double[][] colunaMedia(double[][] matrix) {
+        int linhas = matrix.length;
+        int colunas = matrix[0].length;
+        double[][] mediaColuna = new double[linhas][1];
+        double[] media = new double[linhas];
+        for (int i = 0; i < linhas; i++) {
+            double soma = 0;
+            for (int j = 0; j < colunas; j++) {
+                soma = soma + matrix[i][j];
+            }
+            media[i] = soma/colunas;
+            mediaColuna[i][0] = media[i];
+        }
+        return mediaColuna;
+    }
+
+}

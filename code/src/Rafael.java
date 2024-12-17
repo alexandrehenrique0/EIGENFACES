6edb2f7065895bf6b2906611a73da66179f41593
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-12-17 11:31:50 +0000

    Update funcionalidade 3 rafael test

--- code/src/Rafael.java
+++ code/src/Rafael.java
@@ -1,5 +1,17 @@
 public class Rafael {
-
+    public static void main(String[] args){
+        double[][] matrizVetores = {
+            {1, 8, 1, 10, 9},
+            {2, 3, 2, 5, 6},
+            {3, 4, 3, 6, 7},
+            {4, 5, 4, 7, 8},
+            {8, 6, 5, 8, 9}
+        };
+        double[] vetorPrincipal = {1, 2, 3, 4, 5};
+        double[] resultado = calculate_Euclidian_Distance(vetorPrincipal, matrizVetores);
+        int min_Pos = check_Closer_Vetor(resultado);
+        System.out.println("O vetor mais próximo é o vetor " + min_Pos);
+    }
     public static double[] calculate_Euclidian_Distance(double[] vetorPrincipal, double[][] matrizVetores) {
         double[] resultado = new double[matrizVetores[0].length];
         for (int i = 0; i < matrizVetores[0].length; i++) {
